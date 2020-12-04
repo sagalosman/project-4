2cad38060627bca60cef96db535bdc4bd332b515
Rebecca Acio-Adea <rebeccaacioadea@yahoo.com>
2020-12-04 13:35:00 +0000

    friday_changes

--- backend/serializers/book.py
+++ backend/serializers/book.py
@@ -2,7 +2,7 @@ from app import ma
 from serializers.base import BaseSchema
 from serializers.age import AgeSchema
 from serializers.genre import GenreSchema
-# from serializers.user import UserSchema
+from serializers.user import UserSchema
 from marshmallow import fields
 from models.book import Book
 
@@ -16,5 +16,5 @@ class BookSchema(ma.SQLAlchemyAutoSchema, BaseSchema):
   comments = fields.Nested('CommentSchema', many=True)
   ages = fields.Nested('AgeSchema', many = True)
   genres = fields.Nested('GenreSchema', many = True)
-  # users = fields.Nested('UserSchema', only =('id', 'username'))
+  users = fields.Nested('UserSchema', only =('id', 'username'))
 
