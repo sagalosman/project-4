e80afad07c6cb70a69fc41d42ce4250410bcaf3c
Rebecca Acio-Adea <rebeccaacioadea@yahoo.com>
2020-12-04 12:15:28 +0000

    friday_changes

--- backend/controllers/books.py
+++ backend/controllers/books.py
@@ -2,11 +2,12 @@ from flask import Blueprint, request
 from models.book import Book
 from models.comment import Comment
 from models.age import Age
-# from models.genre import Genre
+from models.genre import Genre
 from serializers.book import BookSchema
 from serializers.comment import CommentSchema
 from serializers.age import AgeSchema
-# from serializers.genre import GenreSchema 
+from serializers.genre import GenreSchema 
+from marshmallow import ValidationError
 
 
 
@@ -26,3 +27,32 @@ def index():
   books = Book.query.all()
 
   return book_schema.jsonify(books, many=True), 200
+
+
+# GET a single book
+
+@router.route('/books/<int:id>', methods=['GET'])
+def get_single_book(id):
+  
+  book = Book.query.get(id) 
+
+  if not book:
+    return { 'message': 'Book not found!' }
+
+  return book_schema.jsonify(book), 200
+
+# CREATE a book
+
+@router.route('/books', methods=['POST'])
+def create():
+  book_dictionary = request.get_json()
+
+  try: 
+    book = book_schema.load(book_dictionary)
+  
+  except ValidationError as e:
+    return { 'errors': e.messages, 'message': 'Something went wrong!'}
+  
+  book.save()
+
+  return book_schema.jsonify(book), 200
--- backend/serializers/book.py
+++ backend/serializers/book.py
@@ -1,5 +1,8 @@
 from app import ma
 from serializers.base import BaseSchema
+from serializers.age import AgeSchema
+from serializers.genre import GenreSchema
+# from serializers.user import UserSchema
 from marshmallow import fields
 from models.book import Book
 
@@ -11,3 +14,7 @@ class BookSchema(ma.SQLAlchemyAutoSchema, BaseSchema):
     load_instance = True
 
   comments = fields.Nested('CommentSchema', many=True)
+  ages = fields.Nested('AgeSchema', many = True)
+  genres = fields.Nested('GenreSchema', many = True)
+  # users = fields.Nested('UserSchema', only =('id', 'username'))
+
