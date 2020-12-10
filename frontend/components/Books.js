416192e5b50702fa6df786f5a0319214914d387a
James Bolton <copse2209@gmail.com>
2020-12-10 14:46:23 +0000

    Fixes errors that stopped singlebook page

--- frontend/components/Books.js
+++ frontend/components/Books.js
@@ -1,5 +1,5 @@
 
-import React, {  useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Bulma from 'bulma'
@@ -20,34 +20,34 @@ const Books = (props) => {
         console.log(resp.data)
       })
   }, [])
-​
+
   console.log(books)
-​
-  return <div className = "section">
-​
+
+  return <div className="section">
+
     {/* <h2> Books Page</h2> */}
     <div className="columns is-multiline is-mobile">
-{books.map((book, index) => {
-  return <div className="column is-one-third-desktop is-half-tablet is-half-mobile" 
-    key={index}> 
-    <Link to={`/books/${book.id}`} >
-    <section class="cards">  
-<article class="card card--2">
-{/* <div class="card__info-hover"> */}
-{/* </div>  */}
-{/* <div class="card__img"></div> */}
-<a href="#" class="card_link">
-<img src={book.image} alt={book.title} />
-<div class="card__img--hover"></div>
-</a> 
-<div class="card__info">
-<h1 class="card__title">{book.title}</h1>
-<span class="card__by">by <a href="#" class="card__author" title="author">{book.author}</a></span>
+      {books.map((book, index) => {
+        return <div className="column is-one-third-desktop is-half-tablet is-half-mobile"
+          key={index}>
+          <Link to={`/books/${book.id}`} >
+            <section class="cards">
+              <article class="card card--2">
+                {/* <div class="card__info-hover"> */}
+                {/* </div>  */}
+                {/* <div class="card__img"></div> */}
+                <a href="#" class="card_link">
+                  <img src={book.image} alt={book.title} />
+                  <div class="card__img--hover"></div>
+                </a>
+                <div class="card__info">
+                  <h1 class="card__title">{book.title}</h1>
+                  <span class="card__by">by <a href="#" class="card__author" title="author">{book.author}</a></span>
 
-</div>
-</article>  
-</section>
-      {/* <div className="card">
+                </div>
+              </article>
+            </section>
+            {/* <div className="card">
         <div className="card-content">
           <div className="media">
             <div className="media-content">
@@ -66,11 +66,11 @@ const Books = (props) => {
           </figure>
         </div>
       </div>  */}
-    </Link>
-  </div>
-})}  
+          </Link>
+        </div>
+      })}
     </div>
   </div>
 }
-​
+
 export default Books
