32b14e8231457918e88e9d7521d44fd62f9025d2
sagalosman <sagalosman0@gmail.com>
2020-12-10 10:36:39 +0000

    test

--- frontend/components/Books.js
+++ frontend/components/Books.js
@@ -2,7 +2,6 @@ import React, {  useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Bulma from 'bulma'
-import SearchBar from './SearchBar'
 
 
 const Books = () => {
@@ -20,29 +19,13 @@ const Books = () => {
   console.log(books)
 
   return <div className = "section">
-
     {/* <h2> Books Page</h2> */}
     <div className="columns is-multiline is-mobile">
       {books.map((book, index) => {
         return <div className="column is-one-third-desktop is-half-tablet is-half-mobile" 
           key={index}> 
           <Link to={`/books/${book.id}`} >
-          <section class="cards">  
-<article class="card card--2">
-  {/* <div class="card__info-hover"> */}
-   {/* </div>  */}
-  {/* <div class="card__img"></div> */}
-   <a href="#" class="card_link">
-   <img src={book.image} alt={book.title} />
-     <div class="card__img--hover"></div>
-   </a> 
-  <div class="card__info">
-    <h1 class="card__title">{book.title}</h1>
-    <span class="card__by">by <a href="#" class="card__author" title="author">{book.author}</a></span>
-  </div>
-</article>  
-  </section>
-            {/* <div className="card">
+            <div className="card">
               <div className="card-content">
                 <div className="media">
                   <div className="media-content">
@@ -60,12 +43,12 @@ const Books = () => {
                   <img src={book.image} alt={book.title} />
                 </figure>
               </div>
-            </div>  */}
+            </div> 
           </Link>
         </div>
       })} 
     </div>
-   </div>
+  </div>
 }
 
 export default Books
